a8471bc16e023cb683b37654cea5a7283d9ee294
Moss <moss@ubuntu.ubuntu-domain>
2014-03-14 15:59:26 +0100

    Implementation of the socket on the server side.

--- serveur/app.js
+++ serveur/app.js
@@ -7,4 +7,13 @@ var server = http.createServer(function(req, res){
 
 var io = require('socket.io').listen(server);
 
+io.sockets.on('connection', function(socker){
+    socket.emit('recept', 'data received');
+    socket.on('userInfo', function(message){
+	console.log('data : ' + message);
+    });
+});
+
 server.listen(8080);
+
+console.log('Listen on port 8080');
