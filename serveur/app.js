10d839ef143f088d0c7e32b0c39c46b1cfb170ad
Ligol <ligol@free.fr>
2014-03-14 17:03:01 +0200

    Send index.html during the connexion

--- serveur/app.js
+++ serveur/app.js
@@ -3,6 +3,10 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
+	    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
 });
 
 var io = require('socket.io').listen(server);
