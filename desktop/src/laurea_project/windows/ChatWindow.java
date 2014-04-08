da4a100cb22e91d32fa4a94065d08ef5a99acbc6
ardrit_g <guillaume.ardrit@epitech.eu>
2014-04-08 13:06:29 +0300

    add of Chat Controller

---
+++ desktop/src/laurea_project/windows/ChatWindow.java
@@ -0,0 +1,45 @@
+package laurea_project.windows;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ChatWindow extends JFrame implements ActionListener {
+
+	private JButton		send;
+	private JTextField	message = new JTextField();
+	
+
+	public ChatWindow(String title) {
+		super(title);
+		setLayout(new FlowLayout());
+		setSize(400, 300);
+		setButton();
+		setTextField();
+		add(send);
+		send.setSize(100, 100);
+		add(message);
+	}
+
+	private void setButton() {
+		// positionnement du bouton a faire
+		send = new JButton("Envoyer");
+		send.addActionListener(this);
+	}
+	
+	private void setTextField() {
+		message.setSize(100, 50);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		send.setSize(150, 150);
+		message.setSize(150, 150);
+	}
+
+}
