900ec2f6884ad5d1b72bcadb3ef63e0295ba7018
Shauny <ludovic.tanter@epitech.eu>
2014-04-08 11:27:26 +0300

    Ajout de la base Desktop

---
+++ desktop/src/my/test/TestUI.java
@@ -0,0 +1,14 @@
+package my.test;
+
+import javax.swing.JFrame;
+
+public class TestUI {
+
+	public static void main(String[] args) {
+		// TODO Instancier une JFrame
+		JFrame fenetre = new JFrame();
+		// TODO Afficher la JFrame
+		fenetre.setVisible(true);
+	}
+
+}
