5eec9eb55c7adc8b3bf8b20905b329db5eaae91f
Shauny <ludovic.tanter@epitech.eu>
2014-04-08 12:28:11 +0300

    Create the ContactWindow and its View

---
+++ desktop/src/laurea_project/ViewContactWindow.java
@@ -0,0 +1,14 @@
+package laurea_project;
+
+import javax.swing.JFrame;
+
+import laurea_project.windows.ContactWindow;
+
+public class ViewContactWindow {
+
+	public static void main(String[] args) {
+		ContactWindow window = new ContactWindow();
+		window.setVisible(true);
+	}
+
+}
