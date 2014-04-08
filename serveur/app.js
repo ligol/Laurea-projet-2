f72d54dacec305a9668e148dd67331c225a53d87
Moss <moss@ubuntu.ubuntu-domain>
2014-04-08 11:25:42 +0200

    Check if the user is disconnected

--- serveur/app.js
+++ serveur/app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var HashMap = require('hashmap').HashMap;
 
 var server = http.createServer(function(req, res){
-	    fs.readFile('./index.html', 'utf-8', function(error, content) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
@@ -17,49 +17,66 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
     socket.emit('recept', 'data received');
     socket.on('userInfo', function(message){
-		console.log('data : ' + message);
-		var response = JSON.parse(message);
-		for (var i = 0; i < response.public.length; i++){
-		    map.set(response.public[i], socket);
-		    if (follow.get(response.public[i]) != null) {
-		    	var client = follow.get(response.public[i]);
-		    	console.log('follow : ' + client);
-		    	for (var i = 0; i < client.length; i++) {
-		    		client[i].emit('connected', '{ \'user\':' + response.public[i] + ', \'state\':' + true);
-		    	};
-		    }
-		}
+	console.log('data : ' + message);
+	var response = JSON.parse(message);
+	for (var i = 0; i < response.public.length; i++){
+	    map.set(response.public[i], socket);
+	    if (follow.get(response.public[i]) != null) {
+		var client = follow.get(response.public[i]);
+		console.log('follow : ' + client);
+		for (var i = 0; i < client.length; i++) {
+		    client[i].emit('connected', '{ \'user\':' + response.public[i] + ', \'state\':' + true);
+		};
+	    }
+	}
     });
     socket.on('message', function(message){
-	    var response = JSON.parse(message);
-	    map.get(response.key).emit('message', message);
-	});
-	socket.on('isConnected', function(message){
-	    var response = JSON.parse(message);
-	    if (map.get(response.key) == null)
-	    	socket.emit('isConnected', false);
-	    else
-	    	socket.emit('isConnected', true);
-	});
+	var response = JSON.parse(message);
+	map.get(response.key).emit('message', message);
+    });
+    socket.on('isConnected', function(message){
+	var response = JSON.parse(message);
+	if (map.get(response.key) == null)
+	    socket.emit('isConnected', false);
+	else
+	    socket.emit('isConnected', true);
+    });
     socket.on('userFollow', function(message){
-		console.log('data : ' + message);
-		var response = JSON.parse(message);
-		for (var i = 0; i < response.public.length; i++) {
-			if (follow.get(response.public[i]) == null) {
-				var array = [];
-				array.push(socket);
-				follow.set(response.public[i], array);
-			}
-			else
-			{
-		    	follow.get(response.public[i]).push(socket);
-				socket.emit('connected', '{ \'user\':' + response.public[i] + ', \'state\':' + true);
-			}
-		}
+	console.log('data : ' + message);
+	var response = JSON.parse(message);
+	for (var i = 0; i < response.public.length; i++) {
+	    if (follow.get(response.public[i]) == null) {
+		var array = [];
+		array.push(socket);
+		follow.set(response.public[i], array);
+	    }
+	    else
+	    {
+		follow.get(response.public[i]).push(socket);
+		socket.emit('connected', '{ \'user\':' + response.public[i] + ', \'state\':' + true);
+	    }
+	}
+    });
+    socket.on('disconnect', function(message) {
+	console.log('Disconnected from the server');
+	follow.forEach(function(value, key) {
+	    console.log("value : "+ value);
+	    console.log("socket : "+ socket);
+	    if (value === socket){
+		console.log(key);
+	    	follow.remove(key);
+	    }
 	});
-	socket.on('disconnect',function() {
-		console.log('Server has disconnected');
-	});
+	var response = JSON.parse(message);
+	for (var i = 0; i < response.public.length; i++){
+	    if (follow.get(response.public[i]) != null) {
+		var client = follow.get(response.public[i]);
+		for (var i = 0; i < client.length; i++) {
+		    client[i].emit('disconnected', '{ \'user\':' + response.public[i] + ', \'state\':' + true);
+		};
+	    }
+	}
+    });
 });
 
 server.listen(8080);
